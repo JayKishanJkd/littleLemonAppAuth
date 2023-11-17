53b46b8ca1c9d71ce0e1c822b3247374126a9abe
jaykishan438 <jaykishandaiya438@gmail.com>
2023-11-17 15:17:59 +0530

    Your commit message here

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const { Pool } = require('pg');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // PostgreSQL database configuration
 const pool = new Pool({
@@ -22,6 +22,75 @@ app.get('/', (req, res) => {
     res.json({ message: ' this is updated testing for kamal daiya' });
   });
 
+  let menuItems = [];
+
+  // Endpoint to get all menu items
+  app.get('/api/menuItems', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT * FROM menu_items');
+      const menuItems = result.rows;
+      res.json(menuItems);
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+  });
+  
+  // Endpoint to add an item to the cart and store it in the database
+  app.post('/api/addToCart', async (req, res) => {
+    const { serial_number, quantity } = req.body;
+    const menuItem = menuItems.find((item) => item.serial_number === serial_number);
+  
+    if (!menuItem) {
+      return res.status(404).json({ success: false, message: 'Item not found' });
+    }
+  
+    const client = await pool.connect();
+  
+    try {
+      await client.query('BEGIN');
+  
+      // Insert the item into the cart table
+      const result = await client.query(
+        'INSERT INTO cart (serial_number, name, category, price_inr, description, quantity) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+        [
+          menuItem.serial_number,
+          menuItem.name,
+          menuItem.category,
+          menuItem.price_inr,
+          menuItem.description,
+          quantity,
+        ]
+      );
+  
+      await client.query('COMMIT');
+  
+      const cartItem = result.rows[0];
+      res.json({ success: true, message: 'Item added to the cart', cartItem });
+    } catch (error) {
+      await client.query('ROLLBACK');
+      console.error('Error:', error);
+      res.status(500).json({ success: false, message: 'Internal server error' });
+    } finally {
+      client.release();
+    }
+  });
+  
+  // Endpoint to get the items in the cart from the database
+  app.get('/api/getCart', async (req, res) => {
+    const client = await pool.connect();
+  
+    try {
+      const result = await client.query('SELECT * FROM cart');
+      const cartItems = result.rows;
+      res.json(cartItems);
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ success: false, message: 'Internal server error' });
+    } finally {
+      client.release();
+    }
+  });
 // Sign-up endpoint
 app.post('/signup', async (req, res) => {
   const { username, password, email } = req.body;
@@ -56,9 +125,9 @@ app.post('/login', async (req, res) => {
   try {
     // Retrieve user information from the database
     const query = `
-      SELECT user_id, password_hash
-      FROM littleLemon.user_authentication
-      WHERE username = $1`;
+    SELECT user_id, password_hash
+    FROM littleLemon.user_authentication
+    WHERE username = $1`;
     
     const result = await pool.query(query, [username]);
 
